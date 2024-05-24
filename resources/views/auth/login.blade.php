507d05a529cecceeb5aab8e50ddf08a55cfb8f0e
Tiago Lima <tiagoepitanga10@gmail.com>
2024-05-24 03:04:54 -0300

    Tela de dashboard iniciada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,26 +15,21 @@
             <form method="POST" action="{{ route('auth') }}" class="w-[20rem] h-[1rem]">
                 @csrf
                 <div class="mb-4">
-                    <label for="email" class="block text-sm font-medium text-gray-700 font-semibold">E-mail</label>
+                    <label for="email" class="block text-sm text-gray-700 font-semibold">E-mail</label>
                     <div class="flex items-center justify-center">
                         <img src="{{ asset('img/usuarioicon.png') }}" alt="" class="mr-2 w-6 h-6">
-                        <input type="email" name="email" id="email"
-                            class="mt-1 block w-full border h-8 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm hover:animate-spin"
-                            required autofocus>
+                        <input type="email" name="email" id="email" class="mt-1 block w-full h-8 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-2 shadow-md" required autofocus>
                     </div>
                 </div>
                 <div class="mb-4">
-                    <label for="password" class="block text-sm font-medium text-gray-700 font-semibold">Senha</label>
+                    <label for="password" class="block text-sm text-gray-700 font-semibold">Senha</label>
                     <div class="flex items-center justify-center">
                         <img src="{{ asset('img/cadeadoicon.png') }}" alt="" class="mr-2 w-6 h-6">
-                        <input type="password" name="password" id="password"
-                            class="h-8 mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm hover:animate-spin"
-                            required>
+                        <input type="password" name="password" id="password" class="h-8 mt-1 block w-full border-2 border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm " required>
                     </div>
                 </div>
                 <div class="w-full items-center justify-center flex">
-                    <button type="submit"
-                        class="w-[13rem] py-2 px-4 border border-transparent rounded-3xl shadow-sm text-sm font-medium text-white bg-[#3E3A4E] hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 justify-center items-center">Entrar</button>
+                    <button type="submit" class="w-[13rem] py-2 px-4 border border-transparent rounded-3xl shadow-md text-sm font-medium text-white bg-[#3E3A4E] hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 justify-center items-center">Entrar</button>
                 </div>
                 <div class="flex justify-between">
                     <div class="mt-4 text-sm">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,19 +2,100 @@
 
 @section('content')
 
-    @if (isset($moedas))
-        <select name="moeda_id" id="moeda_id">
-            @foreach ($moedas as $moeda)
-                <option value="{{ $moeda->id }}">{{ $moeda->descricao }}</option>
-            @endforeach
-        </select>
-    @endif
+<?php
+$moedas = [
+    (object) [
+        'id' => 1,
+        'descricao' => 'Dólar',
+        'simbolo' => 'US$'
+    ],
+    (object) [
+        'id' => 2,
+        'descricao' => 'Euro',
+        'simbolo' => '€'
+    ],
+    (object) [
+        'id' => 3,
+        'descricao' => 'Libra Esterlina',
+        'simbolo' => '£'
+    ],
+    (object) [
+        'id' => 4,
+        'descricao' => 'Iene',
+        'simbolo' => '¥'
+    ]
+];
 
+?>
+
+<body class="h-screen w-screen bg-[#DDDBDA] flex flex-row">
+    <div class="w-[22rem] h-full bg-[#56BE7C] flex flex-col p-10">
+        <div class="w-full flex flex-col justify-center items-center">
+            {imagem do usuario ou:}<img src="{{ asset('img/noUser.png') }}" alt="Logo Money Track" class="rounded-full w-32 h-32">
+        </div>
+        <div class="justify-center items-center flex flex-col">
+            <h1 class="font-bold text-zinc-700 text-2xl p-0 m-2">Tiago Pitanga 10</h1>
+            <h2 class="font-semibold text-zinc-600 text-md p-0">tiagoepitanga10@gmail.com</h2>
+        </div>
+        <div class="flex justify-center items-start ml-8 flex-col gap-10 mt-10">
+            @if (isset($moedas))
+            @foreach ($moedas as $moeda)
+            <button value="{{ $moeda->id }}" class="bg-none">
+                <span class="font-bold text-3xl text-zinc-800">{{$moeda->simbolo}}</span>
+                <span class="font-bold text-2xl text-zinc-700">{{ $moeda->descricao }}</span>
+            </button>
+            @endforeach
+            @endif
+        </div>
+        <div class="flex h-full justify-end flex-col items-center">
+            <button class="font-bold text-xl text-zinc-800 bg-red-400 rounded-lg px-6 py-2 flex flex-row !justify-between items-center w-[14rem] hover:bg-red-500">
+                Desconectar
+                <img src="{{ asset('img/exit.png') }}" alt="Logo Money Track" class="w-6 h-6">
+            </button>
+            <h3 class="font-semibold text-zinc-700 mt-4">BOBBY JONES SOLUTIONS®</h3>
+        </div>
+    </div>
+    <div class="w-[calc(100vw-25rem)] flex items-start flex-col gap-20">
+        <div class="flex flex-row justify-between items-center w-full">
+            <img src="{{ asset('img/logoSemFundo.png') }}" alt="Logo Money Track" class="rounded-lg w-32 h-32 mt-2 ml-6">
+            <h1 class="font-semibold text-zinc-700">DASHBOARD</h1>
+            <button>
+                <img src="{{ asset('img/configIcon.png') }}" alt="Logo Money Track" class="rounded-lg w-10 h-10 mr-6 hover:animate-spin">
+            </button>
+        </div>
+        <div class="w-full flex flex-row mx-10 gap-5 justify-center">
+            <div class="flex flex-col bg-[#56BE7C] w-[18rem] h-[10rem] p-4 gap-5 rounded-md shadow-md border-2 border-white">
+                <h2 class="font-semibold text-zinc-700 text-lg">Saldo atual:</h2>
+                <h3 class="font-bold text-zinc-700 text-2xl">R$ 10.589,00</h3>
+            </div>
+            <div class="flex flex-col bg-white w-[18rem] h-[10rem] p-4 gap-5 rounded-md shadow-md border-2 border-white">
+                <h2 class="font-semibold text-zinc-700 text-lg">Convertidos em {moeda selecionada}:</h2>
+                <h3 class="font-bold text-zinc-700 text-2xl">$ 2.058,27</h3>
+            </div>
+            <div class="flex flex-col bg-white w-[18rem] h-[10rem] p-4 gap-5 rounded-md shadow-md border-2 border-white">
+                <h2 class="font-semibold text-zinc-700 text-lg">Adicionar saldo:</h2>
+                <h3 class="font-bold text-zinc-700 text-2xl">R$ 0,00</h3>
+                <div class="w-full justify-between items-center flex pb-10">
+                    <button class="bg-[#FF443A] text-white font-semibold rounded-md p-2 hover:bg-red-600 h-8">Cancelar</button>
+                    <button class="bg-[#38A263] text-white font-semibold rounded-md p-2 hover:bg-green-700 h-8">Confirmar</button>
+                </div>
+            </div>
+            <div class="flex flex-col bg-white w-[18rem] h-[10rem] p-4 gap-5 rounded-md shadow-md border-2 border-white">
+                <h2 class="font-semibold text-zinc-700 text-lg">Retirar saldo:</h2>
+                <h3 class="font-bold text-zinc-700 text-2xl">R$ 0,00</h3>
+                <div class="w-full justify-between items-center flex pb-10">
+                    <button class="bg-[#FF443A] text-white font-semibold rounded-md p-2 hover:bg-red-600 h-8">Cancelar</button>
+                    <button class="bg-[#38A263] text-white font-semibold rounded-md p-2 hover:bg-green-700 h-8">Confirmar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<!--
     <button id="get_cotacao">Buscar</button>
 
     <div>
         <table id="tbl_cotacoes">
-
             <head>
                 <tr>
                     <th>Data Hora</th>
@@ -25,37 +106,37 @@
             <body>
             </body>
         </table>
-    </div>
-    <script>
-        $("#get_cotacao").click(async function() {
-            const moeda_id = $("#moeda_id").find(":selected").val();
+    </div> -->
+<script>
+    $("#get_cotacao").click(async function() {
+        const moeda_id = $("#moeda_id").find(":selected").val();
 
-            let cotacoes = await getCotacoes(moeda_id);
-            cotacoes = cotacoes.data;
-            console.log(cotacoes);
+        let cotacoes = await getCotacoes(moeda_id);
+        cotacoes = cotacoes.data;
+        console.log(cotacoes);
 
-            const $tbl = $("#tbl_cotacoes tbody");
+        const $tbl = $("#tbl_cotacoes tbody");
 
-            $tbl.html("");
+        $tbl.html("");
 
-            let data = cotacoes.map((item) => {
-                return `<tr><td>${item.datahora}</td><td>${item.valor}</td><tr>`;
-            });
-
-            $tbl.append(data);
+        let data = cotacoes.map((item) => {
+            return `<tr><td>${item.datahora}</td><td>${item.valor}</td><tr>`;
         });
 
+        $tbl.append(data);
+    });
 
-        async function getCotacoes(moeda_id) {
-            try {
-                const response = await fetch(root + `/api/getCotacoes?moeda_id=${moeda_id}`);
-               const data = await response.json();
 
-                return data;
-            } catch (err) {
-                console.error(err);
- 
-            }
+    async function getCotacoes(moeda_id) {
+        try {
+            const response = await fetch(root + `/api/getCotacoes?moeda_id=${moeda_id}`);
+            const data = await response.json();
+
+            return data;
+        } catch (err) {
+            console.error(err);
+
         }
-    </script>
+    }
+</script>
 @endsection
